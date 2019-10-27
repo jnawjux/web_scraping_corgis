9d8adbf95376a0c75c87474a2db3af9fa0bae7e1
John Naujoks <johnnaujoks@gmail.com>
2019-10-26 20:08:16 -0700

    Change insta_details function to only take one link, instead of a list

--- insta_scrape.py
+++ insta_scrape.py
@@ -3,7 +3,7 @@ import re
 from selenium.webdriver import Chrome
 
 
-def recent_25_posts(username, post_count=25):
+def recent_post_links(username, post_count=25):
     """
     With the input of an account page, scrape the 25 most recent posts urls
 
@@ -31,7 +31,7 @@ def recent_25_posts(username, post_count=25):
         return post_links[:post_count]
 
 
-def insta_details(urls):
+def insta_link_details(url):
     """
     Take a post url and return post details
 
@@ -42,23 +42,20 @@ def insta_details(urls):
     A list of dictionaries with details for each Instagram post, including link,
     like/view count, age (when posted), and initial comment
     """
-    browser = Chrome()
-    post_details = []
-    for link in urls:
-        browser.get(link)
-        try:
-            # This captures the standard like count.
-            likes = browser.find_element_by_partial_link_text(' likes').text
-        except:
-            # This captures the like count for videos which is stored
-            view_id = '//*[@id="react-root"]/section/main/div/div/article/div[2]/section[2]/div/span'
-            likes = browser.find_element_by_xpath(view_id).text
-        age = browser.find_element_by_css_selector('a time').text
-        xpath_c = '//*[@id="react-root"]/section/main/div/div/article/div[2]/div[1]/ul/li[1]/div/div/div'
-        comment = browser.find_element_by_xpath(xpath_c).text
-        post_details.append(
-            {'link': link, 'likes/views': likes, 'age': age, 'comment': comment})
-        time.sleep(10)
+    browser = Chrome().get(url)
+    try:
+        # This captures the standard like count.
+        likes = browser.find_element_by_partial_link_text(' likes').text
+    except:
+        # This captures the like count for videos which is stored
+        view_id = '//*[@id="react-root"]/section/main/div/div/article/div[2]/section[2]/div/span'
+        likes = browser.find_element_by_xpath(view_id).text
+    age = browser.find_element_by_css_selector('a time').text
+    xpath_c = '//*[@id="react-root"]/section/main/div/div/article/div[2]/div[1]/ul/li[1]/div/div/div'
+    comment = browser.find_element_by_xpath(xpath_c).text
+    post_details = {'link': link, 'likes/views': likes,
+                    'age': age, 'comment': comment}
+    time.sleep(10)
     return post_details
 
 
